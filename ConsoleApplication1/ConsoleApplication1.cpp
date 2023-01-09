60504f32c208d447e356c460ee0e0ab01c4a6499
Rindrays <fobos201155@mail.ru>
2023-01-10 00:33:53 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	char sign;
+	bool t;
+	double num1, num2, result;
+	cout << "enter number 1 \n";
+	cin >> num1;
+	cout << "enter sign \n";
+	cin >> sign;
+	if ((sign == '+') or (sign == '-') or (sign == '*') or (sign == '+') or (sign == '/')) { t = 1; }
+	else { cout << "error: wrong symbol"; t = 0; }
+	if (t == 1) {
+		cout << "enter number 2 \n";
+		cin >> num2;
+		switch (sign)
+		{
+		case '+':
+		{
+			result = num1 + num2;
+			break;
+		}
+		case '-':
+		{
+			result = num1 - num2;
+			break;
+		}
+		case '*':
+		{
+			result = num1 * num2;
+			break;
+		}
+		case '/':
+		{
+			result = num1 / num2;
+			break;
+		}
+		}
+		if ((sign == '/') && (num1 == 0)) { cout << "error: division by zero"; }
+		else { cout << "result=" << result; }
+		cin.get();
+		cin.get();
+	}
+	else {
+		cin.get();
+		cin.get();
+	}
+}
